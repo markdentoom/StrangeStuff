4afd555997ce3c56315f7e5dd0047897dc991218
Mark den Toom <markdentoom@hotmail.com>
2020-05-15 12:48:42 +0200

    Specified ALLOWED_HOSTS

--- StrangeStuff/settings.py
+++ StrangeStuff/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'obfuscated'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['strangestuffdeployment.eu.pythonanywhere.com']
 
 
 # Application definition
